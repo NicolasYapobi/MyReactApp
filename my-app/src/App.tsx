af23464bef97f644472d7d35720d6a3eb8e98842
Nicolas <nicolas.yapobi@epitech.eu>
2021-03-19 15:01:16 +0100

    Add popup button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,41 @@
 import * as React from 'react';
 import logo from './logo.svg';
+import Popup from 'reactjs-popup';
 import './App.css';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="Home">
-        <div className="button">
-          <button onClick={() => console.log('test')}>ADD</button>
+
+const App = () => (
+  <div>
+    <h1>Home page</h1>
+    <Popup
+      trigger={<button className="button"> Open Modal </button>}
+      modal
+    >
+      {close => (
+        <div className="modal">
+          <a className="close" onClick={close}>
+            &times;
+          </a>
+          <div className="header"> Modal Title </div>
+          <div className="content">
+            {" "}
+              Add your page
+          </div>
+          <div className="actions">
+            <button
+              className="button"
+              onClick={() => {
+                console.log("modal closed ");
+                close();
+              }}
+            >
+              close modal
+            </button>
+          </div>
         </div>
-      </div>
-    );
-  }
-}
+      )}
+    </Popup>
+  </div>
+);
 
 export default App;
